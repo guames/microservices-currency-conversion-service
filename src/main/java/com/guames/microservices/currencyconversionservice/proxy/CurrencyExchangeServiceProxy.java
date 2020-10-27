2fc4d7e9e05e4469b8f257a1d50474ca0ced1da0
games <gustavo.ames87@gmail.com>
2020-10-26 21:44:51 -0300

    Eureka and Ribbon configuration.

--- src/main/java/com/guames/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/guames/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -1,13 +1,15 @@
 package com.guames.microservices.currencyconversionservice.proxy;
 
 import com.guames.microservices.currencyconversionservice.protocols.ExchangeValueResponse;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8082")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("from/{from}/to/{to}")
+    @GetMapping("currency-exchange/from/{from}/to/{to}")
     ExchangeValueResponse retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
